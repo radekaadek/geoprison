399c346c8dd80c9d16e77700ac65df0a87e1f87a
radekaadek <radekaadek@gmail.com>
2025-03-19 17:05:42 +0100

    tile layers

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,20 +1,37 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { browser } from '$app/environment';
-  
-  let map;
+
+  async function loadMap() {
+    // Import Leaflet only on the client side
+    const L = await import('leaflet');
+    
+    // Create map after the component is mounted
+    let map = L.map('map').setView([52, 20], 7);
+          
+    let osmTopoLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    let cartoDarkLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20
+    })
+
+    // Add the default layer to the map
+    osmTopoLayer.addTo(map);
+
+    let tileLayers = {
+      "OpenStreetMap": osmTopoLayer,
+      "CartoDark": cartoDarkLayer
+    }
+
+    let layerControl = L.control.layers(tileLayers).addTo(map);
+  }
   
   onMount(async () => {
     if (browser) {
-      // Import Leaflet only on the client side
-      const L = await import('leaflet');
-      
-      // Create map after the component is mounted
-      map = L.map('map').setView([52, 20], 7);
-      
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
+      loadMap();
     }
   });
 </script>
